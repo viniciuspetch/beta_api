d42e38f2552f757b582a39dd15c0cab3d3183cf7
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-06-08 14:18:11 -0300

    Improve character-story seeding

--- database/seeds/CharacterStorySeed.php
+++ database/seeds/CharacterStorySeed.php
@@ -14,9 +14,15 @@ class CharacterStorySeed extends Seeder
     {
         DB::table('character_story')->insert(['character_id' => 1, 'story_id' => 1]);
         DB::table('character_story')->insert(['character_id' => 1, 'story_id' => 2]);
-        DB::table('character_story')->insert(['character_id' => 2, 'story_id' => 2]);
-        DB::table('character_story')->insert(['character_id' => 2, 'story_id' => 3]);
-        DB::table('character_story')->insert(['character_id' => 3, 'story_id' => 3]);
-        DB::table('character_story')->insert(['character_id' => 3, 'story_id' => 1]);
+        DB::table('character_story')->insert(['character_id' => 1, 'story_id' => 3]);
+        DB::table('character_story')->insert(['character_id' => 1, 'story_id' => 4]);
+        DB::table('character_story')->insert(['character_id' => 2, 'story_id' => 5]);
+        DB::table('character_story')->insert(['character_id' => 2, 'story_id' => 6]);
+        DB::table('character_story')->insert(['character_id' => 2, 'story_id' => 7]);
+        DB::table('character_story')->insert(['character_id' => 2, 'story_id' => 8]);
+        DB::table('character_story')->insert(['character_id' => 3, 'story_id' => 9]);
+        DB::table('character_story')->insert(['character_id' => 3, 'story_id' => 10]);
+        DB::table('character_story')->insert(['character_id' => 3, 'story_id' => 11]);
+        DB::table('character_story')->insert(['character_id' => 3, 'story_id' => 12]);
     }
 }
