053a5006296a38ab5c5a60ff7ce17abbfffa9f79
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-06-08 20:37:44 -0300

    Initial implementation of Passport

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,21 @@ use Illuminate\Support\Facades\DB;
 |
 */
 
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+
+    Route::group([
+        'middleware' => 'auth:api'
+    ], function () {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+});
+
+
 // Characters
 Route::get('/characters', function (Request $request) {
     $characters = DB::table('characters')
