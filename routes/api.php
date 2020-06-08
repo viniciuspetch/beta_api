e45405a56be2ff079d097c2378ce478376029701
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-06-08 00:10:08 -0300

    Created more basic endpoints

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,51 @@ Route::get('/characters', function (Request $request) {
     return response()->json($characters);
 });
 Route::get('/characters/{id}', function (Request $request, $id) {
-    return "/characters//".$id;
+    $character = DB::table('characters')->where('id', $id)->first();
+    return response()->json($character);
+});
+//
+Route::get('/creators', function (Request $request) {
+    $characters = DB::table('creators')->get();
+    return response()->json($characters);
+});
+Route::get('/creators/{id}', function (Request $request, $id) {
+    $character = DB::table('creators')->where('id', $id)->first();
+    return response()->json($character);
+});
+//
+Route::get('/events', function (Request $request) {
+    $characters = DB::table('events')->get();
+    return response()->json($characters);
+});
+Route::get('/events/{id}', function (Request $request, $id) {
+    $character = DB::table('events')->where('id', $id)->first();
+    return response()->json($character);
+});
+//
+Route::get('/series', function (Request $request) {
+    $characters = DB::table('series')->get();
+    return response()->json($characters);
+});
+Route::get('/series/{id}', function (Request $request, $id) {
+    $character = DB::table('series')->where('id', $id)->first();
+    return response()->json($character);
+});
+//
+Route::get('/comics', function (Request $request) {
+    $characters = DB::table('comics')->get();
+    return response()->json($characters);
+});
+Route::get('/comics/{id}', function (Request $request, $id) {
+    $character = DB::table('comics')->where('id', $id)->first();
+    return response()->json($character);
+});
+//
+Route::get('/stories', function (Request $request) {
+    $characters = DB::table('stories')->get();
+    return response()->json($characters);
+});
+Route::get('/stories/{id}', function (Request $request, $id) {
+    $character = DB::table('stories')->where('id', $id)->first();
+    return response()->json($character);
 });
