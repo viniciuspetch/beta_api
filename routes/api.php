53063064cf42b3254923a950066af711e4e4c76c
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-06-08 10:15:48 -0300

    Renamed variables

--- routes/api.php
+++ routes/api.php
@@ -25,46 +25,50 @@ Route::get('/characters/{id}', function (Request $request, $id) {
 });
 //
 Route::get('/creators', function (Request $request) {
-    $characters = DB::table('creators')->get();
-    return response()->json($characters);
+    $creators = DB::table('creators')->get();
+    return response()->json($creators);
 });
 Route::get('/creators/{id}', function (Request $request, $id) {
-    $character = DB::table('creators')->where('id', $id)->first();
-    return response()->json($character);
+    $creators = DB::table('creators')->where('id', $id)->first();
+    return response()->json($creators);
 });
 //
 Route::get('/events', function (Request $request) {
-    $characters = DB::table('events')->get();
-    return response()->json($characters);
+    $events = DB::table('events')->get();
+    return response()->json($events);
 });
 Route::get('/events/{id}', function (Request $request, $id) {
-    $character = DB::table('events')->where('id', $id)->first();
-    return response()->json($character);
+    $events = DB::table('events')->where('id', $id)->first();
+    return response()->json($events);
 });
 //
 Route::get('/series', function (Request $request) {
-    $characters = DB::table('series')->get();
-    return response()->json($characters);
+    $series = DB::table('series')->get();
+    return response()->json($series);
 });
 Route::get('/series/{id}', function (Request $request, $id) {
-    $character = DB::table('series')->where('id', $id)->first();
-    return response()->json($character);
+    $series = DB::table('series')->where('id', $id)->first();
+    return response()->json($series);
 });
 //
 Route::get('/comics', function (Request $request) {
-    $characters = DB::table('comics')->get();
-    return response()->json($characters);
+    $comics = DB::table('comics')->get();
+    foreach($comics as $comic) {
+        $comic->stories = DB::table('stories')->where("stories.comic_id", $comic->id)->get();
+    }
+    return response()->json($comics);
 });
 Route::get('/comics/{id}', function (Request $request, $id) {
-    $character = DB::table('comics')->where('id', $id)->first();
-    return response()->json($character);
+    $comic = DB::table('comics')->where('id', $id)->first();
+    $comic->stories = DB::table('stories')->where("stories.comic_id", $comic->id)->get();
+    return response()->json($comic);
 });
 //
 Route::get('/stories', function (Request $request) {
-    $characters = DB::table('stories')->get();
-    return response()->json($characters);
+    $stories = DB::table('stories')->get();
+    return response()->json($stories);
 });
 Route::get('/stories/{id}', function (Request $request, $id) {
-    $character = DB::table('stories')->where('id', $id)->first();
-    return response()->json($character);
+    $stories = DB::table('stories')->where('id', $id)->first();
+    return response()->json($stories);
 });
