c811c1af0c3495ec0041c90712f511b0a21c69c6
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-06-08 15:52:38 -0300

    Added series HTTP methods

--- routes/api.php
+++ routes/api.php
@@ -316,18 +316,54 @@ Route::delete('/events/{id}', function ($id) {
 
 
 // Series
-Route::get('/series', function (Request $request) {
-    $series = DB::table('series')->get();
+Route::get('/series', function () {
+    $series = DB::table('series')
+        ->get();
     foreach ($series as $sr) {
-        $sr->comics = DB::table("comics")->where("comics.series_id", $sr->id)->get();
+        $sr->comics = DB::table("comics")
+            ->where("comics.series_id", $sr->id)
+            ->get();
     }
     return response()->json($series);
 });
-Route::get('/series/{id}', function (Request $request, $id) {
-    $series = DB::table('series')->where('id', $id)->first();
+Route::get('/series/{id}', function ($id) {
+    $series = DB::table('series')
+        ->where('id', $id)
+        ->first();
     if ($series) {
-        $series->comics = DB::table("comics")->where("comics.series_id", $id)->get();
+        $series->comics = DB::table("comics")
+            ->where("comics.series_id", $id)
+            ->get();
         return response()->json($series);
+    } else {
+        return response()->json([], 404);
+    }
+});
+Route::post('/series', function (Request $request) {
+    if (isset($request->name)) {
+        DB::table('series')
+            ->insert(["name" => $request->name, "created_at" => now(), "updated_at" => now()]);
+        return response()->json([], 200);
+    } else {
+        return response()->json([], 400);
+    }
+});
+Route::put('/series/{id}', function (Request $request, $id) {
+    if (isset($request->name)) {
+        DB::table('series')
+            ->where("id", $id)
+            ->update(["name" => $request->name, 'updated_at' => now()]);
+        return response()->json([], 200);
+    } else {
+        return response()->json([], 404);
+    }
+});
+Route::delete('/series/{id}', function ($id) {
+    $series = DB::table("series")
+        ->where('id', $id);
+    if ($series->first()) {
+        $series->delete();
+        return response()->json([], 200);
     } else {
         return response()->json([], 404);
     }
