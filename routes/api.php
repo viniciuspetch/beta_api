ec9bbe231120097aeda121cd94e3e27a1549391e
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-06-08 14:38:53 -0300

    Added more endpoints to character

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,8 @@ use Illuminate\Support\Facades\DB;
 
 // Characters
 Route::get('/characters', function (Request $request) {
-    $characters = DB::table('characters')->get();
+    $characters = DB::table('characters')
+    ->get();
     foreach ($characters as $character) {
         $character->creators = DB::table("character_creator")
             ->where('character_id', $character->id)
@@ -57,7 +58,8 @@ Route::get('/characters', function (Request $request) {
 });
 Route::get('/characters/{id}', function (Request $request, $id) {
     $character = DB::table('characters')
-        ->where('id', $id)->first();
+        ->where('id', $id)
+        ->first();
     if ($character) {
         $character->creators = DB::table("character_creator")
             ->where('character_id', $character->id)
@@ -93,6 +95,89 @@ Route::get('/characters/{id}', function (Request $request, $id) {
             ->groupBy('events.id')
             ->get();
         return response()->json($character);
+    } else {
+        return response()->json([], 404);
+    }
+});
+Route::get('/characters/{id}/creators', function ($id) {
+    $character = DB::table('characters')
+        ->where('id', $id)
+        ->first();
+    if ($character) {
+        $creators = DB::table("character_creator")
+            ->where('character_id', $character->id)
+            ->join('creators', 'character_creator.creator_id', '=', 'creators.id')
+            ->select(['creators.id as id', 'creators.name', 'creators.created_at', 'creators.updated_at'])
+            ->get();
+        return response()->json($creators);
+    } else {
+        return response()->json([], 404);
+    }
+});
+Route::get('/characters/{id}/stories', function ($id) {
+    $character = DB::table('characters')
+        ->where('id', $id)
+        ->first();
+    if ($character) {
+        $stories = DB::table("character_story")
+            ->where('character_id', $character->id)
+            ->join('stories', 'character_story.story_id', '=', 'stories.id')
+            ->select(['stories.id as id', 'stories.name as name', 'stories.created_at as created_at', 'stories.updated_at as updated_at'])
+            ->get();
+        return response()->json($stories);
+    } else {
+        return response()->json([], 404);
+    }
+});
+Route::get('/characters/{id}/comics', function ($id) {
+    $character = DB::table('characters')
+        ->where('id', $id)
+        ->first();
+    if ($character) {
+        $comics = DB::table("character_story")
+            ->where('character_id', $character->id)
+            ->join('stories', 'character_story.story_id', '=', 'stories.id')
+            ->join('comics', 'stories.comic_id', '=', 'comics.id')
+            ->select(['comics.id as id', 'comics.name as name', 'comics.created_at as created_at', 'comics.updated_at as updated_at'])
+            ->groupBy('comics.id')
+            ->get();
+        return response()->json($comics);
+    } else {
+        return response()->json([], 404);
+    }
+});
+Route::get('/characters/{id}/series', function ($id) {
+    $character = DB::table('characters')
+        ->where('id', $id)
+        ->first();
+    if ($character) {
+        $series = DB::table("character_story")
+            ->where('character_id', $character->id)
+            ->join('stories', 'character_story.story_id', '=', 'stories.id')
+            ->join('comics', 'stories.comic_id', '=', 'comics.id')
+            ->join('series', 'comics.series_id', '=', 'series.id')
+            ->select(['series.id as id', 'series.name as name', 'series.created_at as created_at', 'series.updated_at as updated_at'])
+            ->groupBy('series.id')
+            ->get();
+        return response()->json($series);
+    } else {
+        return response()->json([], 404);
+    }
+});
+Route::get('/characters/{id}/events', function ($id) {
+    $character = DB::table('characters')
+        ->where('id', $id)
+        ->first();
+    if ($character) {
+        $events = DB::table("character_story")
+            ->where('character_id', $character->id)
+            ->join('stories', 'character_story.story_id', '=', 'stories.id')
+            ->join('event_story', 'stories.id', '=', 'event_story.story_id')
+            ->join('events', 'event_story.event_id', '=', 'events.id')
+            ->select(['events.id as id', 'events.name as name', 'events.created_at as created_at', 'events.updated_at as updated_at'])
+            ->groupBy('events.id')
+            ->get();
+        return response()->json($events);
     } else {
         return response()->json([], 404);
     }
