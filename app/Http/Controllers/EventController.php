f6d3d8c13b50931b2faf44461d63d61f53373557
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-06-09 20:13:26 -0300

    Organization

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class EventController extends Controller
 {
